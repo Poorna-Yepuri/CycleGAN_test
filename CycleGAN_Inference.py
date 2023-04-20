7c21531b200acdaab2305e1b18799d9d408464ad
Prasanna Kumar Y <pyepuri1@student.gsu.edu>
2023-04-20 14:10:47 -0400

    Updated Inference

--- CycleGAN_Inference.py
+++ CycleGAN_Inference.py
@@ -10,6 +10,7 @@ from keras.models import load_model
 from matplotlib import pyplot
 from numpy.random import randint
 from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.preprocessing.image import array_to_img
 from tensorflow.keras.utils import load_img
 from matplotlib import pyplot as plt
 import numpy as np
@@ -18,13 +19,13 @@ from sklearn.utils import resample
 from Resize_Images import upscale, downsize
 
 
-# select a random sample of images from the dataset
-def select_sample(dataset, n_samples):
-	# choose random instances
-	ix = randint(0, dataset.shape[0], n_samples)
-	# retrieve selected images
-	X = dataset[ix]
-	return X
+# # select a random sample of images from the dataset
+# def select_sample(dataset, n_samples):
+# 	# choose random instances
+# 	ix = randint(0, dataset.shape[0], n_samples)
+# 	# retrieve selected images
+# 	X = dataset[ix]
+# 	return X
 
 # plot the image, its translation, and the reconstruction
 def show_plot(imagesX, imagesY1, imagesY2):
@@ -44,19 +45,19 @@ def show_plot(imagesX, imagesY1, imagesY2):
 		pyplot.title(titles[i])
 	pyplot.show()
 
-# load dataset
-A_data = resample(dataA, 
-                 replace=False,     
-                 n_samples=50,    
-                 random_state=42) # reproducible results
+# # load dataset
+# A_data = resample(dataA, 
+#                  replace=False,     
+#                  n_samples=50,    
+#                  random_state=42) # reproducible results
 
-B_data = resample(dataB, 
-                 replace=False,     
-                 n_samples=50,    
-                 random_state=42) # reproducible results
+# B_data = resample(dataB, 
+#                  replace=False,     
+#                  n_samples=50,    
+#                  random_state=42) # reproducible results
 
-A_data = (A_data - 512) / 512
-B_data = (B_data - 512) / 512
+# A_data = (A_data - 512) / 512
+# B_data = (B_data - 512) / 512
 
 
 # load the models
@@ -80,32 +81,43 @@ def example_B_A():
 
 ##########################
 #Load a single custom image
-def life_of_photo(infer_img):
+def life_of_photo(infer_img):  # share path of infering image
     test_image = load_img(infer_img)
     test_image = img_to_array(test_image)
     test_image_input = np.array([test_image]) 
-    test_image_input = (test_image_input - 512) / 512
+    test_image_input = (test_image_input - 127.5) / 127.5
 
     # plot B->A->B (Photo to Painting to Photo)
-    painting_generated  = model_BtoA.predict(test_image_input)
-    photo_reconstructed = model_AtoB.predict(painting_generated)
-    show_plot(test_image_input, painting_generated, photo_reconstructed)
+    GAN_painting_generated  = model_BtoA.predict(test_image_input)
+    photo_reconstructed = model_AtoB.predict(GAN_painting_generated)
+    show_plot(test_image_input, GAN_painting_generated, photo_reconstructed)
+
+    generated_img_1 = 0.5 * GAN_painting_generated + 0.5
+    generated_img = array_to_image(generated_img_1[0])
+    return generated_img
 
 def infer_img(img_path):
     img = cv2.imread(img_path)
+    global test_img_shape
     test_img_shape = img.shape
-    if test_img_shape[1] < 1024 and test_img_shape[0] < 1024:
-        inp_img = upscale(img, test_img_shape)
+    if test_img_shape[1] < 256 and test_img_shape[0] < 256:
+        inp_img = upscale(img, (256,256))
     else:
-        inp_img = downsize(img, 1024)
-    
-    test_image = img_to_array(inp_img)
-    test_image_input = np.array([test_image])  # Convert single image to a batch.
-    test_image_input = (test_image_input - 127.5) / 127.5
+        inp_img = downsize(img, 256)
 
-    # plot B->A->B (Photo to Monet to Photo)
-    GAN_generated  = model_BtoA.predict(test_image_input)
-    photo_reconstructed = model_AtoB.predict(GAN_generated)
-    show_plot(test_image_input, GAN_generated, photo_reconstructed)
+    return inp_img
 
+def infer_new_image(input_image):
+    cv2.imwrite("temp.jpg", infer_img(input_image))
+    output_image = life_of_photo("./temp.jpg")
+    save_path = "generatedImages/" + input_image.split("/")[-1] + "_GAN_generated.jpg"
+    output_image.save(save_path)
     
+    resize_img = cv2.imread(save_path)
+    generated_image_resized = cv2.resize(resize_img, (test_img_shape[1],test_img_shape[0]))
+
+    output_path = "resizedOutput/" + input_image.split("/")[-1] + "_GAN_Resized.jpg"
+    cv2.imwrite(output_path, generated_image_resized)
+    
+
+
